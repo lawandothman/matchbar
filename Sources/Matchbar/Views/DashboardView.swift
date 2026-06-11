2a0ee1af52228ea5517727c90369b40384bb8c2b
Lawand Othman <lwnd@pm.me>
2026-06-11 17:05:57 +0100

    Revert "custom full width tabs"
    This reverts commit 386f53c5cddcfb10718d5fe276e6a72e8d8d697f.

--- Sources/Matchbar/Views/DashboardView.swift
+++ Sources/Matchbar/Views/DashboardView.swift
@@ -32,9 +32,16 @@ struct DashboardView: View {
     }
 
     private var tabPicker: some View {
-        TabPicker(selection: $tab)
-            .padding(.horizontal, 12)
-            .padding(.top, 8)
+        Picker("", selection: $tab) {
+            Text("Matches").tag(DashboardTab.matches)
+            Text("Groups").tag(DashboardTab.groups)
+        }
+        .pickerStyle(.segmented)
+        .labelsHidden()
+        .controlSize(.small)
+        .frame(maxWidth: .infinity)
+        .padding(.horizontal, 12)
+        .padding(.top, 8)
     }
 
     private var header: some View {
